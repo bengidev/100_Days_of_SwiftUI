5f23a1f3ee5defccd9c0d02916f9d15fc9a8da6d
syndicate017 <bambang.trd17@gmail.com>
2023-09-17 07:14:01 +0700

    Day 27 BetterRest from 100 Days of SwiftUI

---
+++ Day_27_BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bambang Tri Rahmat Doni on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
