84ad49739729e3ad86609e4be9fe5be8938a01d1
syndicate017 <bambang.trd17@gmail.com>
2023-09-23 09:40:21 +0700

    Day 51 CupcakeCorner from 100 Days of SwiftUI

--- Day_50_CupcakeCorner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ Day_50_CupcakeCorner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -12,6 +12,11 @@ struct CheckoutView: View {
     ///
     @ObservedObject var order: Order
 
+    /// Sending and receiving orders over the internet
+    ///
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         /// Preparing for checkout
         ///
@@ -29,12 +34,45 @@ struct CheckoutView: View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place Order", action: {})
+                Button("Place Order", action: {
+                    Task {
+                        await placeOrder()
+                    }
+                })
                     .padding()
             }
         }
         .navigationTitle("Checkout Details")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank You!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+
+    }
+    
+    /// Sending and receiving orders over the internet
+    ///
+    private func placeOrder() async -> Void {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
--- Day_50_CupcakeCorner/CupcakeCorner/CupcakeCorner/Order.swift
+++ Day_50_CupcakeCorner/CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 /// Taking basic order details
-/// 
-class Order: ObservableObject, Identifiable {
+///
+
+class Order: ObservableObject, Identifiable, Codable {
+    /// Encoding an ObservableObject class
+    ///
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
     static var id = UUID()
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
@@ -62,5 +69,35 @@ class Order: ObservableObject, Identifiable {
         }
 
         return cost
+    }
+    
+    init() {}
+    
+    /// Encoding an ObservableObject class
+    ///
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
     }
 }
